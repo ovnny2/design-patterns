c012adf4a61df370cd96a9095a56ffca26d5ff6c
ovnny2 <ovnnychannel@gmail.com>
2023-08-21 14:19:24 -0300

    iniciando o projeto

---
+++ videocurator/src/main/java/br/com/ovnny/videocurator/VideoCuratorApplication.java
@@ -0,0 +1,13 @@
+package br.com.ovnny.videocurator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCuratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoCuratorApplication.class, args);
+	}
+
+}
---
+++ videocurator/src/test/java/br/com/ovnny/videocurator/VideoCuratorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ovnny.videocurator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCuratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
